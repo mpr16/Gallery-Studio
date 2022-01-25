b024212390f9d0faf47d151d6d093b211ea36f8a
mpr16 <91697439+mpr16@users.noreply.github.com>
2022-01-25 15:46:26 +0100

    Delete vue.config.js

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-// vue.config.js file to be place in the root of your repository
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ?
-    '/practicavue_alejandraretto_martapons/' 
-    : '/'           //Utilizo el nombre de LA CARPETA DE EJEMPLO. TU USA EL TUYO
-    }
-    
